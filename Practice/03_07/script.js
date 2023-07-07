6b54de326b6698a882b9368d60532ed758d3eb35
Lamareeb <104644721+Lamareeb@users.noreply.github.com>
2023-07-07 14:42:04 +0000

    practice creating object

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,43 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const house = {
+    name: "laurasHouse",
+    height: 20,
+    width: 30,
+    length: 25,
+    acres: 1,
+    priceInThousands: 20,
+    test: "iamonlyatest",
+  
+    //nest an object
+    features: {
+      bedrooms: 2,
+      kitchen: "yes",
+      bathrooms: 2,
+      fireplace: "yes",
+      ac: "yes",
+    },
+    //nest another object
+    utilities: {
+      wellWater: "yes",
+      septic: "yes",
+      electricity: "yes",
+    },
+    forSale: true,
+    toggleHouse: function(houseStatus) {
+      this.forSale = houseStatus; //assign forSale status to houseStatus
+    },
+  };
+  console.log("Name: ", house.name);
+  
+  console.log("$", house.priceInThousands, ",000");
+  console.log("Bedrooms: ", house.features.bedrooms);
+  console.log("Bathrooms: ", house.features.bathrooms);
+  console.log("Acres: ", house.acres);
+  
+  console.log("Water: ", house.utilities.wellWater);
+  console.log("Septic: ", house.utilities.septic);
+  console.log("Electricity: ", house.utilities.electricity);
+  
+  console.log("houseforSale", house.forSale);
